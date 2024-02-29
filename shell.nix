60004d25d29680b5bbd908b0d5fec464c926badd
Amy Yan <ayanamydev@gmail.com>
2024-02-29 18:10:09 +1100

    feat: jekyll tidy

---
+++ shell.nix
@@ -0,0 +1,34 @@
+{ pkgs ? import ./pkgs.nix {}, ci ? false }:
+
+with pkgs;
+mkShell {
+  nativeBuildInputs = [
+    ruby.devEnv
+    git
+    pkg-config
+    bundix
+  ];
+  # Don't set rpath for native addons
+  NIX_DONT_SET_RPATH = true;
+  NIX_NO_SELF_RPATH = true;
+  shellHook = ''
+    set -o allexport
+    . ./.env
+    set +o allexport
+    set -v
+    ${
+      lib.optionalString ci
+      ''
+      set -o errexit
+      set -o nounset
+      set -o pipefail
+      shopt -s inherit_errexit
+      ''
+    }
+    mkdir --parents "$(pwd)/tmp"
+
+    export PATH="$HOME/.gems/bin:$PATH"
+
+    set +v
+  '';
+}
