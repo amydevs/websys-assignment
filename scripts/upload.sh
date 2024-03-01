ab74c3b63f8b998ac56c0085a455a9f24fc635d4
Amy Yan <ayanamydev@gmail.com>
2024-03-01 14:35:27 +1100

    feat: edstem upload script

---
+++ scripts/upload.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+site_path=$(readlink -f ./_site/)
+site_path_length=${#site_path}
+
+for file_path in $(find $site_path)
+do
+    if [ $site_path != $file_path ]
+    then
+        dir=$(dirname $file_path)
+        relative_dir=${dir:site_path_length}
+        upload_return=$(curl "https://edstem.org/api/workspaces/$ED_WORKSPACE_ID/upload" \
+        -X POST \
+        -H "content-type: application/json" \
+        -H "Authorization: $ED_API_TOKEN" \
+        --data-raw "{\"wid\":\"$ED_WORKSPACE_ID\",\"path\":\"/home${relative_dir}/dummy\"}")
+
+        ticket=$(echo $upload_return | jq -r '.ticket')
+
+        curl "https://sahara.au.edstem.org/upload/${ticket}" \
+        -X POST \
+        -F upload=@$file_path
+    fi
+done
