2c6ec77279307ac6a6123d654665a33ef145a9f7
Amy Yan <ayanamydev@gmail.com>
2024-03-04 22:28:13 +1100

    feat: added about me

---
+++ scripts/download-images.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+curl \
+    -o ./assets/img/home/gh-languages.svg \
+    "https://github-readme-stats.vercel.app/api/?username=amydevs&title_color=d61b69&bg_color=fbfbfe&hide_border=true"
