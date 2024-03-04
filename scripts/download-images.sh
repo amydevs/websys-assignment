fcf7f0527f26b95ac160cd2159e8ef16a4907f66
Amy Y <ctrlalternativedelete@gmail.com>
2024-03-04 23:51:19 +1100

    fix: github stats instead

--- scripts/download-images.sh
+++ scripts/download-images.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 curl \
-    -o ./assets/img/home/gh-languages.svg \
+    -o ./assets/img/home/gh-stats.svg \
     "https://github-readme-stats.vercel.app/api/?username=amydevs&title_color=d61b69&bg_color=fbfbfe&hide_border=true"
